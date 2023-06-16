63bbdddd3ea4a4e615c28b20e784e7a3e890f933
zhaoxu-lingoace <xu.zhao@pplingo.com>
2023-06-16 11:48:04 +0800

    播放状态

--- src/TalkStatus.vue
+++ src/TalkStatus.vue
@@ -1,11 +1,34 @@
 <script>
+import { VideoPlay, Headset, Mic } from "@element-plus/icons-vue";
+
 export default {
   name: "TalkStatus",
+  data() {
+    return {
+      talkStatus: "none",
+    };
+  },
 };
 </script>
 
 <template>
   <div>
-    <div class="talk-status"></div>
+    <div class="talk-status">
+      <el-icon size="40"
+        ><VideoPlay v-if="talkStatus === 'play'" />
+        <Headset v-if="talkStatus === 'hear'" />
+        <Mic v-if="talkStatus === 'talk'" />
+      </el-icon>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.talk-status {
+  width: 72px;
+  height: 72px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
